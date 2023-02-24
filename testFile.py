c37671ab219b9417f8e334b7f9c7b1cb58dc4497
Melon-10 <17753679317>
2023-02-24 16:03:20 +0800

    测试提交2

--- testFile.py
+++ testFile.py
@@ -4,9 +4,9 @@ def read_excel(file_path,sheet_name):
     wb = openpyxl.load_workbook(file_path)
     sheet_data = wb[sheet_name]
     lines_count = sheet_data.max_row  # 获取总行数
-    print(lines_count)
+    # print(lines_count)
     cols_count = sheet_data.max_column  # 获取总列数
-    print(cols_count)
+    # print(cols_count)
     # 注意openxl读取行喝列时都是从1开始
     # 第一行我们是表头所以我们便利时不读
     data = []
@@ -16,6 +16,7 @@ def read_excel(file_path,sheet_name):
             cell_data = sheet_data.cell(l, c).value
             if cell_data is None:
                 cell_data=""
+            # print(cell_data)
             line.append(cell_data)  # 将当前单元格的数据添加到列表
         data.append(line)           # 将列表中的数据添加到data列表
     return data
